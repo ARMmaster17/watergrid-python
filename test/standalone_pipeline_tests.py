d31588cf7103cd6f4953f6a93fe4676cac986f77
Joshua Zenn <wordman05@gmail.com>
2022-03-29 13:07:12 -0400

    #8 Linting fixes

--- test/standalone_pipeline_tests.py
+++ test/standalone_pipeline_tests.py
@@ -5,5 +5,5 @@ from watergrid.pipelines import StandalonePipeline
 
 class StandalonePipelineTestCases(unittest.TestCase):
     def test_can_initialize_pipeline(self):
-        pipeline = StandalonePipeline('test_pipeline')
-        self.assertEqual(pipeline.get_pipeline_name(), 'test_pipeline')
+        pipeline = StandalonePipeline("test_pipeline")
+        self.assertEqual(pipeline.get_pipeline_name(), "test_pipeline")
